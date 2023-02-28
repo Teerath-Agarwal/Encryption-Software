f0d86c19a337ad16cf8c89f5c888eea2c4f46f5d
Teerath <teerath173@gmail.com>
2023-03-01 03:36:38 +0530

    many changes, features added

--- es_src.cpp
+++ es_src.cpp
@@ -25,18 +25,19 @@ string file_name(){
 
 string new_pass(){
     string p;
-    cout<<"Enter a password for your file: ";
+    cout<<"Enter a password: ";
     cin>>p;
-    system(clear);
+    system(clr);
     string check;
     cout<<"Confirm password: ";
     cin>>check;
-    system(clear);
+    system(clr);
     if (p.compare(check))
     {
         cout<<"Passwords don't match! Try again!\n";
         return new_pass();
     }
+    // p.append(str_code);
     return p;
 }
 
@@ -72,6 +73,27 @@ uint8_t* pass_check_util(string pw){
     return s3;
 }
 
+bool verify_pass(string f, string pw){
+    ifstream read;
+    read.open(f);
+    auto s = pass_check_util(pw);
+    char t;
+    uint8_t x;
+    for (char i=0; i<pwlen; i++){
+        read>>t;
+        x = hex_to_dec(t);
+        x <<= 4;
+        read>>t;
+        x += hex_to_dec(t);
+        if (x!=s[i]) {
+            read.close();
+            return false;
+        }
+    }
+    read.close();
+    return true;
+}
+
 void encrypt(ifstream &inp, string f, string pw){
     string res;
     uint8_t t,k, pw_counter1 = 0, pw_counter2 = 0;
@@ -101,28 +123,7 @@ void encrypt(ifstream &inp, string f, string pw){
         }
     }
     outp.close();
-    cout<<"File encrypted successfully!\n\n";
-}
-
-bool verify_pass(string f, string pw){
-    ifstream read;
-    read.open(f);
-    auto s = pass_check_util(pw);
-    char t;
-    uint8_t x;
-    for (char i=0; i<pwlen; i++){
-        read>>t;
-        x = hex_to_dec(t);
-        x <<= 4;
-        read>>t;
-        x += hex_to_dec(t);
-        if (x!=s[i]) {
-            read.close();
-            return false;
-        }
-    }
-    read.close();
-    return true;
+    return;
 }
 
 void display(ifstream &k){
@@ -136,6 +137,25 @@ void display(ifstream &k){
     return;
 }
 
+void display2(string t){
+    stringstream s(t);
+    string pr;
+    bool check = 1;
+    while (!s.eof()){
+        getline(s,pr);
+        system(clr);
+        cout << pr;
+        cout<<"\n\n\n(Press ENTER...)";
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        if (check){
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            check = 0;
+        }
+    }
+    system(clr);
+    return;
+}
+
 int count_alph(ifstream &inp){
     int t = -1;
     uint8_t c;
@@ -143,11 +163,11 @@ int count_alph(ifstream &inp){
         t++;
         inp>>c;
     }
-    inp.close();
+    // inp.close();
     return t;
 }
 
-string decrypt(ifstream &inp, string f, string pw){
+string decrypt(ifstream &inp, string pw){
     string res;
     char t, pw_counter1 = 0, pw_counter2 = 0;
     uint8_t x;
@@ -170,9 +190,143 @@ string decrypt(ifstream &inp, string f, string pw){
     return res;
 }
 
+string decrypt(string fl, string pw){
+    string res;
+    char t, pw_counter1 = 0, pw_counter2 = 0;
+    uint8_t x;
+    auto s = sha(pw);
+    for (int i = 2*pwlen; i<fl.size();){
+        t = fl[i++];
+        x = hex_to_dec(t);
+        x <<= 4;
+        t = fl[i++];
+        x += hex_to_dec(t);
+        res += (x^s[pw_order[pw_counter1][pw_counter2++]]);
+        if (pw_counter2>=pwlen) {
+            pw_counter2 = 0;
+            pw_counter1++;
+            if (pw_counter1>=100) pw_counter1 = 0;
+        }
+    }
+    return res;
+}
+
 void enter(){
     cout<<"\n\nPress enter to continue...";
     cin.ignore(numeric_limits<streamsize>::max(),'\n');
     cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    return;
+}
+
+string edit_name(string f){
+    int i=f.length()-1;
+    while (f[i]!='.') i--;
+    f.insert(i,"_tl");
+    return f;
+}
+
+int d_to_m(uint8_t m){
+    switch (m){
+        case 1: return 0;
+        case 2: return 31;
+        case 3: return 59;
+        case 4: return 90;
+        case 5: return 120;
+        case 6: return 151;
+        case 7: return 181;
+        case 8: return 212;
+        case 9: return 243;
+        case 10: return 273;
+        case 11: return 304;
+        case 12: return 334;
+        default: return 365;
+    }
+}
+
+uint64_t epoch_conv(uint64_t t){
+    if (!t) return 0;
+    uint64_t ep = 0;
+    uint8_t yrs = ((t/10000)%100);
+    int days = (yrs*365) + ((yrs+3)/4) + 10957;
+    days += d_to_m((t/1000000)%100) + (t/100000000) - 1;
+    if ((!(yrs%4)) && ((t/1000000)%100)>2) days++;
+    ep = (((days*24)+((t/100)%100))*60 + (t%100))*60;
+    ep -= 19800;
+    return ep;
+}
+
+void time_lock(string f, string pw){
+    uint64_t ts,te;
+    string newpw;
+    sleep(1);
+    system(clr);
+    cout<<"\nEnter the time for which file will be unlocked.\nFormat: DDMMYYHHMM, 24-hour format, no spaces. Enter 0 for no limit.\nStart:  ";
+    cin>>ts;
+    cout<<"Ending: ";
+    cin>>te;
+    cout<<"Generalized for time locked file,\n";
+    newpw = new_pass();
+    ts = epoch_conv(ts) + int_code1;
+    te = epoch_conv(te) + int_code2;
+    ofstream outp;
+    outp.open(f);
+    outp << pw << endl;
+    outp << ts << endl;
+    outp << te;
+    outp.close();
+
+    ifstream inp;
+    inp.open(f);
+    inp.unsetf(ios_base::skipws);
+    tc = count_alph(inp);
+    inp.clear();
+    inp.seekg(0);
+    inp.unsetf(ios_base::skipws);
+    encrypt(inp,f,str_code+newpw+str_code+str_code);
+
+    inp.open(f);
+    inp.unsetf(ios_base::skipws);
+    tc = count_alph(inp);
+    inp.clear();
+    inp.seekg(0);
+    inp.unsetf(ios_base::skipws);
+    encrypt(inp,f,newpw+str_code);
+    return;
+}
+
+string time_unlock(ifstream &inp, string pw, string f){
+    stringstream s(decrypt(decrypt(inp,pw+str_code),str_code+pw+str_code+str_code));
+    getline(s,pw);
+    uint64_t ts,te,tn;
+    s>>ts;
+    s>>te;
+    ts -= int_code1;
+    te -= int_code2;
+    tn = time(NULL);
+    if (ts>tn){
+        system(clr);
+        cout<<"You are not allowed to open the file yet!\n";
+        return "0";
+    }
+    if (te && te<tn){
+        system(clr);
+        cout<<"Sorry, it's too late, the files have expired!\n";
+        remove(to_char(edit_name(f)));
+        return "1";
+    }
+    inp.open(f);
+    inp.unsetf(ios_base::skipws);
+    tc = count_alph(inp);
+    inp.clear();
+    inp.seekg(0);
+    inp.unsetf(ios_base::skipws);
+    system(clr);
+    return decrypt(decrypt(inp,pw+str_code),pw);
+}
+
+void readme(){
+    system(clr);
+    cout<<"This software is designed to encrypt and secure files. Default format used is .bin\nThere are several other formats that work, such as .txt and .dat. For Linux platform, .wav (audio) and .mp4 (video) can also be encrypted.\nMost of the options are self explainatory. Note that once a file is encrypted, there is no way to decrypt it if the password is forgotten. Even if the source code is available.\nExplaining option 7 and 8, it is basically a feature where the user can unlock their encrypted file(probably for someone else) for specific period of time, and set a different password for them. This way, user can show their encrypted file to someone else for any period of time (might be in future), and still not tell them the original password, just the password that was set later. This maintains data security."<<endl;
+    enter();
     return;
 }
--- main.cpp
+++ main.cpp
@@ -1,55 +1,55 @@
-// Project Started: 27 March 2022
-// Version 2 started on: 26 November 2022 (major update)
-// Last worked: 1 December 2022
-// formats tested: .bin, .dat, .txt, .wav, .mp4 (audio & video as well)
 #include "ES.h"
 
-int main()
-{
-    while(1)
-    {
+int main(){
+    while(1){
         uint16_t z;
         ifstream inp;
         ofstream outp;
         string f, pw, dec;
 
-        system(clear);
-        cout<<"1. Encrypt a new file\n2. Decrypt a file\n3. Display an encrypted file\n4. Display a decrypted file\n5. Create a new file\n6. Exit\n-> ";
+        system(clr);
+        cout<<"1. Encrypt a new file\n2. Decrypt a file\n3. Display an encrypted file\n\n4. Display a file\n5. Create a new file\n6. Remove a file\n\n7. Time-lock a file (Assuming it's aleady encrypted)\n8. Show a time locked file\n\n9. Open ReadMe\n10. Exit\n\n-> ";
         cin>>z;
 
-        if (z>=1 && z<=4)
-        {
+        if ((z>=1 && z<=4) || z==6 || z==7 || z==8){
             f = file_name();
             inp.open(f);
 
-            if (inp.fail())
-            {
+            if (inp.fail()){
                 cout<<"Error! This file does not exist!\n\n";
                 continue;
             }
-            else {
+            
+            if (z==8){
+                inp.close();
+                dec = f;
+                f = edit_name(f);
+                inp.open(f);
+            }
+            
+            if ((z>=1 && z<=4) || z==8){
                 inp.unsetf(ios_base::skipws);
                 tc = count_alph(inp);
-                // cout<<"tc = "<<tc<<endl;
-                inp.open(f);
+                inp.clear();
+                inp.seekg(0);
                 inp.unsetf(ios_base::skipws);
                 cout<<"File opened successfully!\n";
             }
         }
-        else if (z!=5)
-        {
+        else if (z>=10){
             cout<<"\nThank You! Have a beautiful day!\nCode written by Teerath Agarwal";
             enter();
             return 0;
         }
+        
 
         bool pass = 0;
-        if (z==2 || z==3){
+        if (z==2 || z==3 || z==7 || z==8){
             bool rep = 1;
             while (rep){
                 cout<<"Password: ";
                 cin>>pw;
-                if (verify_pass(f,pw)){
+                if (verify_pass(f,pw+str_code)){
                     cout<<"Password Verified!\n";
                     rep = 0;
                 }
@@ -66,15 +66,22 @@ int main()
         }
         if (pass) continue;
 
-        switch (z)
-        {
+        switch (z){
             case 1:
                 pw = new_pass();
                 encrypt(inp,f,pw);
+                inp.open(f);
+                inp.unsetf(ios_base::skipws);
+                tc = count_alph(inp);
+                inp.clear();
+                inp.seekg(0);
+                inp.unsetf(ios_base::skipws);
+                encrypt(inp,f,pw+str_code);
+                cout<<"File encrypted successfully!\n\n";
                 sleep(1);
                 break;
             case 2:
-                dec = decrypt(inp,f,pw);
+                dec = decrypt(decrypt(inp,pw+str_code),pw);
                 remove(to_char(f));
                 outp.open(f);
                 outp << dec;
@@ -83,14 +90,14 @@ int main()
                 sleep(1);
                 break;
             case 3:
-                dec = decrypt(inp,f,pw);
-                system(clear);
+                dec = decrypt(decrypt(inp,pw+str_code),pw);
+                system(clr);
                 cout<<dec;
                 cout<<"\n\n\nText displayed above is the decrypted file";
                 enter();
                 break;
             case 4:
-                system(clear);
+                system(clr);
                 display(inp);
                 enter();
                 break;
@@ -102,7 +109,23 @@ int main()
                 outp.open(f);
                 outp << dec;
                 outp.close();
-                sleep(1);
+                break;
+            case 6:
+                remove(to_char(f));
+                break;
+            case 7:
+                inp.close();
+                f = edit_name(f);
+                time_lock(f,pw);
+                break;
+            case 8:
+                dec = time_unlock(inp, pw, dec);
+                if (!(dec.length()==1 && (dec[0]=='1' || dec[0]=='0')))
+                cout<<dec;
+                enter();
+                break;
+            case 9:
+                readme();
                 break;
         }
     }
